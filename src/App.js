f204b9937b9f0cbf2e9b5379758421e70f395294
Snurkeburk <snurkeburk@gmail.com>
2022-06-26 18:06:40 +0200

    basically everything

--- src/App.js
+++ src/App.js
@@ -27,17 +27,34 @@ import { HandleUser } from "./compontents/Auth/HandleUser";
 import { getAuth } from "firebase/auth";
 import CreateProfile from "./pages/CreateProfile";
 import PageNotFound from "./pages/PageNotFound";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import Cookies from "js-cookie";
+import { CheckOnline } from "./compontents/Auth/CheckOnline";
 function App() {
+  window.addEventListener(
+    "beforeunload",
+    () => {
+      CheckOnline(false, authentication.currentUser.uid);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    { once: true }
+  );
   const [isSignedIn, setIsSignedIn] = useState(false);
   const [reqUsername, setReqUsername] = useState(false);
   useEffect(() => {
+    // if user exits browser, sign them out
+
     HandleUser().then((re) => {
       if (re != "req-username") {
         setIsSignedIn(re);
-        console.log("ALLOWED");
+        // set cookie with uid
+        if (re) {
+          const uid = authentication.currentUser.uid;
+          CheckOnline(isSignedIn, authentication.currentUser.uid);
+          Cookies.set("uid", uid);
+        }
       } else {
         setReqUsername(true);
-        console.log("REQ USERNAME");
       }
     });
   });
